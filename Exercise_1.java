1adfd9fdbde41e0a1f703dd056a34bb5f7d39a0b
4NUJ-5H4RM4 <46234603+4NUJ-5H4RM4@users.noreply.github.com>
2024-05-11 13:27:54 +0000

    completed pre course 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,28 +8,47 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top <0 ;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top==MAX-1){
+           System.out.println("stack overflow");
+           return false;
+        }
+        else{
+            top++;
+            a[top]=x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()){
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        else {
+            return a[top--];
+        }
         //Write your code here
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
